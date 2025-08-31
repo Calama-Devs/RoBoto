afbfd5b05201f8aab6bb3d66abe1a6e2a7457793
Leo Ferrarezi <leoferrarezi@gmail.com>
2025-08-30 22:55:48 -0400

    Teste de CI

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -225,6 +225,8 @@
 
     <div class="heroe">
 
+        <h1>Let's to it, guys!</h1>
+
         <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
         <h2>The small framework with powerful features</h2>
