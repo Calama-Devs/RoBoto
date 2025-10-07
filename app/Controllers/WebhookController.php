abdea472811038c83862ee9993359d995d7f765f
Leo Ferrarezi <leoferrarezi@gmail.com>
2025-10-07 09:58:38 -0400

    teste

--- app/Controllers/WebhookController.php
+++ app/Controllers/WebhookController.php
@@ -5,8 +5,11 @@ class WebhookController extends BaseController {
         //
     }
 
-    public function response() {
+    public function response()
+    {
         $dados = $this->request->getJSON(true);
+
+        echo "<pre>" . json_encode($dados) . "</pre>";
 
         //Teste
         log_message('debug', 'Webhook recebido: ' , ['dados' => json_encode($dados)]);
