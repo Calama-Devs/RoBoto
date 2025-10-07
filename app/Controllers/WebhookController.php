7590e695346e99395f67481ae4cdc90ea6a93f95
Leo Ferrarezi <leoferrarezi@gmail.com>
2025-10-07 10:14:26 -0400

    a

--- app/Controllers/WebhookController.php
+++ app/Controllers/WebhookController.php
@@ -9,7 +9,14 @@ class WebhookController extends BaseController {
     {
         $dados = $this->request->getJSON(true);
 
-        print_r($dados);
+        if($dados['event'] === 'messages.upsert')
+        {
+            $contato = $dados['data']['key']['remoteJid']; //Número do contato
+            $mensagem = $dados['data']['message']['conversation']; //Mensagem recebida
+
+            echo "Contato: " . $contato . "\n";
+            echo "Mensagem: " . $mensagem . "\n";        
+        }
 
         //Teste
         //log_message('error', 'Webhook recebido: ' . json_encode($dados));
