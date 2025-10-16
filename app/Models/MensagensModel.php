e877dad5d82cc2f49bf9f4e14272c673962dc87f
Ana Clara Gibson <anaclarasouzagibson@gmail.com>
2025-10-16 17:27:10 -0300

    comenta a validacao para teste

--- app/Models/MensagensModel.php
+++ app/Models/MensagensModel.php
@@ -12,41 +12,41 @@ class MensagensModel extends Model
     protected $returnType = 'array';
     protected $useSoftDeletes = false;
     protected $protectFields = true;
-    protected $allowedFields = ['contato_id', 'texto', 'remetente', 'score_filtro', 'sessao_id', 'timestamp'];
+    protected $allowedFields = ['contato_id', 'texto', 'remetente'];
     protected bool $allowEmptyInserts = false;
 
     //Dates
     protected $useTimestamps = true;
 
     //Validation
-    protected $validationRules = [
-      'contato_id'   => 'numeric|max_length[15]',
-      'texto'        => 'required|string',
-      'remetente'    => 'max_length[100]',
-      'score_filtro' => 'numeric',
-      'sessao_id'    => 'numeric|max_length[11]',
-    ];
-
-    protected $validationMessages = [
-        'contato_id'   => [
-            'numeric'    => 'O contato deve ser numérico.',
-            'max_length' => 'O contato deve ter no máximo 15 caracteres',
-        ],
-        'texto'        => [
-            'required' => 'O campo texto é obrigatório',
-            'string'   => 'O campo texto deve ser texto',
-        ],
-        'remetente'    => [
-            'max_length' => 'O remetente deve ter no máximo 100 caracteres',
-        ],
-        'score_filtro' => [
-            'numeric' => 'O campo score_filtro deve ser numérico'
-        ],
-        'sessao_id'    => [
-            'numeric'    => 'O campo sessao_id deve ser numérico',
-            'max_length' => 'O campo sessao_id deve ter no máximo 11 caracteres.'
-        ],
-    ];
+//    protected $validationRules = [
+//      'contato_id'   => 'numeric|max_length[15]',
+//      'texto'        => 'required|string',
+//      'remetente'    => 'max_length[100]',
+//      'score_filtro' => 'numeric',
+//      'sessao_id'    => 'numeric|max_length[11]',
+//    ];
+//
+//    protected $validationMessages = [
+//        'contato_id'   => [
+//            'numeric'    => 'O contato deve ser numérico.',
+//            'max_length' => 'O contato deve ter no máximo 15 caracteres',
+//        ],
+//        'texto'        => [
+//            'required' => 'O campo texto é obrigatório',
+//            'string'   => 'O campo texto deve ser texto',
+//        ],
+//        'remetente'    => [
+//            'max_length' => 'O remetente deve ter no máximo 100 caracteres',
+//        ],
+//        'score_filtro' => [
+//            'numeric' => 'O campo score_filtro deve ser numérico'
+//        ],
+//        'sessao_id'    => [
+//            'numeric'    => 'O campo sessao_id deve ser numérico',
+//            'max_length' => 'O campo sessao_id deve ter no máximo 11 caracteres.'
+//        ],
+//    ];
 
     public function create(string $mensagem) {
         $dados = [
